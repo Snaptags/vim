80d22a2b6f44c6c36675e0282bdc917e4109944f
Markus Lasermann <markus.lasermann@ptvgroup.com>
2017-07-24 12:18:18 +0200

    add vterm.lib to build script

--- build.cmd
+++ build.cmd
@@ -3,6 +3,9 @@ if /i [%1] == [clean] (
   call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Make_mvc.mak clean
 
 ) else (
+  pushd .\libvterm
+  call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Makefile.msc
+  pushd ..\
   call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Make_mvc.mak
 )
 popd
