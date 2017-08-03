da8f151fb490bffb9d1e358ee8aead1db69c68b7
Markus Lasermann <markus.lasermann@ptvgroup.com>
2017-08-03 10:58:50 +0200

    fix broken build

--- build.cmd
+++ build.cmd
@@ -3,9 +3,6 @@ if /i [%1] == [clean] (
   call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Make_mvc.mak clean
 
 ) else (
-  pushd .\libvterm
-  call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Makefile.msc
-  pushd ..\
   call %VS_DIR%\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\nmake -f Make_mvc.mak
 )
 popd
