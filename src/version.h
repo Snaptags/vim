1530f260018dddf2a08ee16b573e82b66859a83e
Markus Lasermann <markus.lasermann@ptvgroup.com>
2017-11-06 10:44:00 +0100

    v8.0.1272

--- src/version.h
+++ src/version.h
@@ -19,9 +19,9 @@
 #define VIM_VERSION_MINOR_STR		"0"
 #define VIM_VERSION_100	    (VIM_VERSION_MAJOR * 100 + VIM_VERSION_MINOR)
 
-#define VIM_VERSION_BUILD		1207
+#define VIM_VERSION_BUILD		1272
 #define VIM_VERSION_BUILD_BCD		0x119
-#define VIM_VERSION_BUILD_STR		"1207"
+#define VIM_VERSION_BUILD_STR		"1272"
 #define VIM_VERSION_PATCHLEVEL		 0
 #define VIM_VERSION_PATCHLEVEL_STR	"0"
 /* Used by MacOS port should be one of: development, alpha, beta, final */
@@ -34,7 +34,7 @@
  * VIM_VERSION_LONG is used for the ":version" command and "Vim -h".
  */
 #define VIM_VERSION_NODOT	"vim80"
-#define VIM_VERSION_SHORT	"8.0.1207"
-#define VIM_VERSION_MEDIUM	"8.0.1207"
-#define VIM_VERSION_LONG	"VIM - Vi IMproved 8.0.1207 (2017 Oct 20)"
-#define VIM_VERSION_LONG_DATE	"VIM - Vi IMproved 8.0.1207 (2017 Oct 20, compiled "
+#define VIM_VERSION_SHORT	"8.0.1272"
+#define VIM_VERSION_MEDIUM	"8.0.1272"
+#define VIM_VERSION_LONG	"VIM - Vi IMproved 8.0.1272 (2017 Nov 06)"
+#define VIM_VERSION_LONG_DATE	"VIM - Vi IMproved 8.0.1272 (2017 Nov 06, compiled "
