4e44934e7dbc34d4ba88a6dd3ae1895ea63f8737
Markus Lasermann <markus.lasermann@ptvgroup.com>
2019-09-02 16:57:11 +0200

    v8.1.1963

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 pushd .\src
 if /i [%1] == [clean] (
-  call %VS_DIR%\VC\Tools\MSVC\14.21.27702\bin\Hostx64\x64\nmake -f Make_mvc.mak clean
+  call %VS_DIR%\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\nmake -f Make_mvc.mak clean
 ) else (
-  call %VS_DIR%\VC\Tools\MSVC\14.21.27702\bin\Hostx64\x64\nmake -f Make_mvc.mak
+  call %VS_DIR%\VC\Tools\MSVC\14.22.27905\bin\Hostx64\x64\nmake -f Make_mvc.mak
 )
 popd
