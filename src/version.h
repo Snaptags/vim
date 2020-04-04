5f85673730a96983bc55dfbb87196ed599471198
Markus Lasermann <markus.lasermann@ptvgroup.com>
2020-04-04 13:17:33 +0200

    v8.2.0508

--- src/version.h
+++ src/version.h
@@ -18,16 +18,16 @@
 // Values that change for a new release.
 #define VIM_VERSION_MAJOR		8
 #define VIM_VERSION_MINOR		2
-#define VIM_VERSION_BUILD		0460
+#define VIM_VERSION_BUILD		0508
 #define VIM_VERSION_BUILD_BCD		0x11b
-#define VIM_VERSION_DATE_ONLY	"2020 Mar 27"
+#define VIM_VERSION_DATE_ONLY	"2020 Apr 04"
 
 // Values based on the above
 #define VIM_VERSION_MAJOR_STR		"8"
 #define VIM_VERSION_MINOR_STR		"2"
 #define VIM_VERSION_100	    (VIM_VERSION_MAJOR * 100 + VIM_VERSION_MINOR)
 
-#define VIM_VERSION_BUILD_STR		"0460"
+#define VIM_VERSION_BUILD_STR		"0508"
 #ifndef VIM_VERSION_PATCHLEVEL
 # define VIM_VERSION_PATCHLEVEL		0
 #endif
